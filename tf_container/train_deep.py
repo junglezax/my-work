2ab9573aec41409aebe4ce5220db33fdf953c308
jiang@mac <jiang@mac>
2018-05-07 22:05:22 +0800

    180507

--- tf_container/train_deep.py
+++ tf_container/train_deep.py
@@ -169,7 +169,7 @@ def main(_):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str,
-                        default='/Users/jiang/data_set/container/cont180430',
+                        default='/Users/jiang/data_set/container/container-20160710_sub',
                         help='Directory for storing input data')
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
